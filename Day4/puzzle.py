ab56a2edc134ffedad870ac232646b5ed59e44db
Alex Hoffman <alxhoff@gmail.com>
2019-12-04 10:41:30 +0100

    Day 4.1

---
+++ Day4/puzzle.py
@@ -0,0 +1,57 @@
+#!/bin/python
+
+
+def get_digit(number, n):
+    return number // 10**n % 10
+
+
+def set_digit(number, n, value):
+    cur_value = get_digit(number, n) * 10**n
+    new_value = value * 10**n
+    return number - cur_value + new_value
+
+
+def increase_password(number):
+    modified = False
+    for i in range(5, -1, -1):
+        digit = get_digit(number, i)
+        next_digit = get_digit(number, i - 1)
+        if next_digit < digit:
+            modified = True
+            for j in range(i + 1):
+                number = set_digit(number, j, digit)
+        elif i == 0 and not modified:
+            if digit == 9:
+                return increase_password(number + 1)
+            else:
+                return number + 1
+    return number
+
+
+def check_password(password):
+    for i in range(5):
+        if get_digit(password, i) == get_digit(password, i + 1):
+            return True
+    return False
+
+
+password_count = 0
+
+#  lower_limit = int(input("Lower limit: "))
+#  upper_limit = int(input("Upper limit: "))
+
+lower_limit = 168630
+
+if not check_password(lower_limit):
+    lower_limit = increase_password(lower_limit)
+
+upper_limit = 718098
+
+current_password = lower_limit
+
+while lower_limit <= current_password <= upper_limit:
+    if check_password(current_password):
+        password_count += 1
+    current_password = increase_password(current_password)
+
+print(password_count)
