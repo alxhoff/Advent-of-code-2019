8f8dbdf4045103cd7a07b9bb9e7dbcf037f767b3
Alex Hoffman <alxhoff@gmail.com>
2019-12-17 16:13:12 +0100

    Day 16.2

--- Day16/puzzle.py
+++ Day16/puzzle.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import math
-import hashlib
+import sys
 
 class FFT:
 
@@ -11,13 +11,16 @@ class FFT:
     def __init__(self, signal=None):
 
         if signal:
-            self.signal = self._arrayInt(int(signal))
+            char_arr = np.array([int(val) for val in signal])
+            self.signal = np.concatenate(np.repeat([char_arr], 10000, axis=0))
         else:
-            self.signal = self._arrayInt(int(input("$>")))
-        self.base_patterns = dict()
+            char_arr = np.array([int(val) for val in input("$>")])
+            self.signal = np.concatenate(np.repeat([char_arr], 10000, axis=0))
 
-    def _getKey(self, val):
-        return hashlib.sha1(str(val).encode()).hexdigest()
+        self.test_signal = np.copy(self.signal)
+        self.offset = self.intArray(self.signal[:7])
+
+        print("Offset: {}".format(self.offset))
 
     def _arrayInt(self, res):
         ret = []
@@ -30,11 +33,6 @@ class FFT:
         len+=1 #as we discard first element
         ret = []
 
-        key = self._getKey([index, len])
-
-        if key in self.base_patterns:
-            return self.base_patterns[key]
-
         for i in range(self.base_repeating_pattern.size):
             add = np.full(index + 1, self.base_repeating_pattern[i])
             ret.append(add)
@@ -42,7 +40,6 @@ class FFT:
 
         if ((index + 1) * 4 - 1) >= len:
             ret = ret[1:len]
-            self.base_patterns[key] = ret
             return ret
         else:
             repeats = math.ceil(len/((index + 1) * 4)) - 1
@@ -50,9 +47,7 @@ class FFT:
             for i in range(repeats):
                 apnd.append(np.copy(ret))
             ret = np.concatenate(apnd)[1:len]
-            self.base_patterns[key] = ret
             return ret
-
 
     def _getReturnArray(self, inp):
 
@@ -66,11 +61,16 @@ class FFT:
         rep = self._getRepeatingPattern(index, array.size)
         return  abs(np.sum(rep * array)) % 10
 
-    def intArray(self):
+    def intArray(self, array=None):
 
         ret = 0
-        for i in range(self.signal.size):
-            ret += self.signal[self.signal.size - i - 1] * 10**i
+
+        if not any(array):
+            for i in range(self.test_signal.size):
+                ret += self.test_signal[self.test_signal.size - i - 1] * 10**i
+        else:
+            for i in range(array.size):
+                ret += array[array.size - i - 1] * 10**i
 
         return ret
 
@@ -78,14 +78,38 @@ class FFT:
 
         for i in range(times):
             ret = []
-            for i in range(len(self.signal)):
-                ret.append(self._multiply(i, self.signal))
+            for i in range(len(self.test_signal)):
+                ret.append(self._multiply(i, self.test_signal))
 
-            self.signal = np.array(ret)
+            self.test_signal = np.array(ret)
+            print(self.test_signal)
+
+    def sumBottomHalf(self, times=10):
+        half = int(self.signal.size / 2)
+
+        for i in range(times):
+            new_signal = np.copy(self.signal)
+            sum = 0
+            for n in reversed(range(half,self.signal.size)):
+                sum += self.signal[n]
+                sum = sum %10
+                new_signal[n] = sum
+
+            self.signal = new_signal
+
+            print("{} : {}".format(i, self.signal))
+
+    def getOffsetValue(self):
+        val = self.signal[self.offset:self.offset+8]
+        return self.intArray(val)
 
 
-fft = FFT(59754835304279095723667830764559994207668723615273907123832849523285892960990393495763064170399328763959561728553125232713663009161639789035331160605704223863754174835946381029543455581717775283582638013183215312822018348826709095340993876483418084566769957325454646682224309983510781204738662326823284208246064957584474684120465225052336374823382738788573365821572559301715471129142028462682986045997614184200503304763967364026464055684787169501819241361777789595715281841253470186857857671012867285957360755646446993278909888646724963166642032217322712337954157163771552371824741783496515778370667935574438315692768492954716331430001072240959235708)
-fft.performFFT(100)
-print(fft.signal)
-print(fft.intArray())
+fft = FFT('59754835304279095723667830764559994207668723615273907123832849523285892960990393495763064170399328763959561728553125232713663009161639789035331160605704223863754174835946381029543455581717775283582638013183215312822018348826709095340993876483418084566769957325454646682224309983510781204738662326823284208246064957584474684120465225052336374823382738788573365821572559301715471129142028462682986045997614184200503304763967364026464055684787169501819241361777789595715281841253470186857857671012867285957360755646446993278909888646724963166642032217322712337954157163771552371824741783496515778370667935574438315692768492954716331430001072240959235708')
+
+# fft.performFFT(100)
+#
+# print("Recursion")
+
+fft.sumBottomHalf(100)
+print(fft.getOffsetValue())
 
